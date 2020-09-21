5bb2b47b0c49a92377d46d3cf5209ea612969795
emmm <1029846268@qq.com>
2020-09-21 22:41:31 +0800

    :art: 优化build提示，template依赖添加eurus，script替换为eurus

--- src/build/index.js
+++ src/build/index.js
@@ -1,3 +1,4 @@
+const chalk = require("chalk");
 const shell = require("shelljs");
 
 module.exports = function build(argv) {
@@ -22,5 +23,20 @@ module.exports = function build(argv) {
   const cmd = `cross-env NODE_ENV=${env}${variables} remax build -t ${argv.t} ${
     argv.w ? "-w" : ""
   }`;
-  shell.exec(cmd);
+
+  console.log(chalk.green.bold("\t\n 开始构建"));
+  console.log(chalk.green.bold(`\t\n 开启 Remax 进程`));
+
+  const child = shell.exec(cmd, { async: true });
+
+  child.stdout.on("data", function (data) {
+    if (data.includes("Compiling")) {
+      console.log(
+        chalk.green.bold(`\t\n 编译中，目标平台为 ${argv.t}，请稍候... \t\n`)
+      );
+    }
+    if (data.includes("Watching")) {
+      console.log(chalk.green.bold(`\t\n 构建完毕，正在监听文件修改...`));
+    }
+  });
 };
